74385abd035d168baabdc850f6688c22dc12b499
Dave Reed <dave256@mac.com>
2021-05-01 20:11:33 -0400

    Revert "fix import statement"
    This reverts commit 761d9a4cf29af8d5ee81ef2ac30d10b5b5ae24d3.

--- SSZipArchive/include/ZipArchive.h
+++ SSZipArchive/include/ZipArchive.h
@@ -19,5 +19,5 @@ FOUNDATION_EXPORT const unsigned char ZipArchiveVersionString[];
 #if COCOAPODS
 #import <SSZipArchive/SSZipArchive.h>
 #else
-#import <SSZipArchive/include/SSZipArchive.h>
+#import <ZipArchive/SSZipArchive.h>
 #endif
